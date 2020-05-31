a5fcfe825c41eb467607a0468eca248fc00b975a
rparks <roger.a.parks@gmail.com>
2020-05-31 12:39:29 -0700

    print RC commands over debug UART too and coded UART messages

--- micropython/main.py
+++ micropython/main.py
@@ -152,9 +152,10 @@ def main(rtc, lcd, sbus_uart, debug_uart):
             rlf_cmd(pos_cmds[11])
 
         debug_uart.write("\x02{}\x03\r\n".format("\t".join(
-            [str(x) for x in pos_cmds + [state_machine.state, len(profiler.position_target_queue), ticks_us()]])))
-        # if rc_command is not None:
-        #     debug_uart.write("\x02{}\x03\r\n".format("\t".join([str(x) for x in rc_command])))
+            [str(x) for x in [0x1] + pos_cmds + [state_machine.state, len(profiler.position_target_queue), ticks_us()]])))
+        if rc_command is not None:
+            debug_uart.write("\x02{}\x03\r\n".format("\t".join(
+                [str(x) for x in [0x2] + rc_command])))
 
 
 if __name__ == '__main__':
